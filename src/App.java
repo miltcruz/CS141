621f94aae3715b5d3c2e39d4e138af9979dc5fbd
Milton Cruz <mr.cruzm@gmail.com>
2026-01-27 13:10:07 -0800

    Added Student class and examples

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        // Create Student objects
+        // toString demonstration
+        // equals and hashCode demonstration
+        Student angie = new Student("Angie", 1, 4.0);
+        Student angi = new Student("Angie", 1, 4.0);
+        System.out.println(angie.toString());
+        Student mike = new Student("Mike", 2, 4.0);
+        System.out.println(String.format("Is Mike equal to Angie? %s", angie.equals(mike)));
+        System.out.println(String.format("Is Angi equal to Angie? %s", angie.equals(angi)));
+        System.out.println(String.format("What's Angi's unique ID? %s", angi.hashCode()));
     }
 }
